6fe3a4b93ec56aa4b14a59bbd4a79ac388d2252f
Isaac Bravo <is.bravo@duocuc.cl>
2024-09-08 17:04:49 -0300

    abanse html bienbenido-home-iniciar-sesion

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,14 +3,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'bienvenidos',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'bienvenidos',
+    loadChildren: () => import('./bienvenidos/bienvenidos.module').then( m => m.BienvenidosPageModule)
   },
+  {
+    path: 'inicio-sesion',
+    loadChildren: () => import('./inicio-sesion/inicio-sesion.module').then( m => m.InicioSesionPageModule)
+  },
+
 ];
 
 @NgModule({
