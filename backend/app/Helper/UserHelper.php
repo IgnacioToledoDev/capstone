790934fc29c40b8ed5d27eb897c43da324afc2f8
Ignacio Toledo <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-09-17 23:05:00 -0300

    fixes

--- backend/app/Helper/UserHelper.php
+++ backend/app/Helper/UserHelper.php
@@ -7,17 +7,17 @@ class UserHelper
 
     public function validateRut($rut): bool
     {
-        $value = str_replace('.', '', $value); // remove char .
-        $value = str_replace('-', '', $value); // remove char -
-        $dv = substr($value, -1);
+        $rut = str_replace('.', '', $rut); // remove char .
+        $rut = str_replace('-', '', $rut); // remove char -
+        $dv = substr($rut, -1);
 
-        $value = substr($value, 0, -1); // remove verified digit
+        $rut = substr($rut, 0, -1); // remove verified digit
 
         $acc = 1;
         $count = 0;
-        while ($value != 0) {
-            $acc = ($acc + (intval($value) % 10) * (9 - $count++ % 6)) % 11;
-            $value = (int)($value / 10);
+        while ($rut != 0) {
+            $acc = ($acc + (intval($rut) % 10) * (9 - $count++ % 6)) % 11;
+            $rut = (int)($rut / 10);
         }
 
         $dvCalculate = $acc ? chr($acc + 47) : 'K';
