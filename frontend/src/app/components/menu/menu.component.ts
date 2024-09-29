c58f473627a0952a4a0c998cb5dbfee3637d9c51
Isaac Bravo <is.bravo@duocuc.cl>
2024-09-29 18:14:59 -0300

    mejorar menu

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
+
 
 @Component({
   selector: 'app-menu',
@@ -7,8 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController, private storage: Storage , private alertController: AlertController,) { }
 
   ngOnInit() {}
+  
+  async cerrarSesion() {
+    console.log('Sesión cerrada');
 
+    this.navCtrl.navigateRoot('/inicio-sesion');
+  }
+  async presentLogoutConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirmar',
+      message: '¿Estás seguro de que deseas cerrar la sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cierre de sesión cancelado');
+          }
+        },
+        {
+          text: 'Cerrar Sesión',
+          handler: () => {
+            this.cerrarSesion(); 
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
