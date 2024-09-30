1cc9cab171fb0f780ad6983affc003bbf6bcead8
Ignacio Toledo <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-09-30 00:05:19 -0300

    feat - avances

--- backend/app/Models/Maintenance.php
+++ backend/app/Models/Maintenance.php
@@ -2,9 +2,43 @@
 
 namespace App\Models;
 
+use Barryvdh\LaravelIdeHelper\Eloquent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ *
+ *
+ * @mixin Eloquent
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property int $status_id
+ * @property int $service_id
+ * @property int|null $actual_mileage
+ * @property string|null $recommendation_action
+ * @property int $pricing
+ * @property int $car_id
+ * @property int $mechanic_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereActualMileage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereCarId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereMechanicId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance wherePricing($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereRecommendationAction($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereServiceId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereStatusId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereUpdatedAt($value)
+ */
 class Maintenance extends Model
 {
     use HasFactory;
--- backend/app/Models/Tenant.php
+++ backend/app/Models/Tenant.php
@@ -5,6 +5,34 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $administrator
+ * @property string $address
+ * @property int|null $number_of_employees
+ * @property string|null $phone_number
+ * @property string|null $email
+ * @property string|null $services_offered
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant whereAdministrator($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant whereNumberOfEmployees($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant whereServicesOffered($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tenant whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Tenant extends Model
 {
     use HasFactory;
