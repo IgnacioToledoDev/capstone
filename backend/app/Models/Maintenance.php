e5f3d22af768cb5d3a64e8c5b612a9723355ec11
Ignacio Toledo <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-09-30 00:05:42 -0300

    chore - rename imports

--- backend/app/Models/Maintenance.php
+++ backend/app/Models/Maintenance.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Barryvdh\LaravelIdeHelper\Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
@@ -23,21 +24,21 @@ use Illuminate\Support\Carbon;
  * @property int $mechanic_id
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance query()
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereActualMileage($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereCarId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereMechanicId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance wherePricing($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereRecommendationAction($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereServiceId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereStatusId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Maintenance whereUpdatedAt($value)
+ * @method static Builder|Maintenance newModelQuery()
+ * @method static Builder|Maintenance newQuery()
+ * @method static Builder|Maintenance query()
+ * @method static Builder|Maintenance whereActualMileage($value)
+ * @method static Builder|Maintenance whereCarId($value)
+ * @method static Builder|Maintenance whereCreatedAt($value)
+ * @method static Builder|Maintenance whereDescription($value)
+ * @method static Builder|Maintenance whereId($value)
+ * @method static Builder|Maintenance whereMechanicId($value)
+ * @method static Builder|Maintenance whereName($value)
+ * @method static Builder|Maintenance wherePricing($value)
+ * @method static Builder|Maintenance whereRecommendationAction($value)
+ * @method static Builder|Maintenance whereServiceId($value)
+ * @method static Builder|Maintenance whereStatusId($value)
+ * @method static Builder|Maintenance whereUpdatedAt($value)
  */
 class Maintenance extends Model
 {
