909d6025362151ecaa32b0ffb6b4709fdfaafab3
Ignacio Toledo <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-10-05 18:33:08 -0300

    fix - view owner name en list cars

--- backend/app/Models/Car.php
+++ backend/app/Models/Car.php
@@ -54,7 +54,7 @@ class Car extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'owner_id');
     }
 
     public function carBrands(): BelongsTo
