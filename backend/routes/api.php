d9ef554b02d75a78d985955c9c85151c58bba371
Ignacio Toledo <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-10-24 20:14:49 -0300

    chore - replace import

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\APIv1Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,4 @@ require __DIR__ . '/api/CarModelsRoutes.php';
 require __DIR__ . '/api/QuotationRoutes.php';
 
 
-Route::get('login', [\App\Http\Controllers\APIv1Controllers\UserController::class, 'login'])->name('login');
+Route::get('login', [UserController::class, 'login'])->name('login');
