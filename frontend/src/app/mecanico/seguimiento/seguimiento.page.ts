dec3a8c29f832d300b9a5004128571016d4416bd
Isaac Bravo <is.bravo@duocuc.cl>
2024-11-07 18:19:56 -0300

    canviar html segimiento

--- frontend/src/app/mecanico/seguimiento/seguimiento.page.ts
+++ frontend/src/app/mecanico/seguimiento/seguimiento.page.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController ,NavController} from '@ionic/angular';
 
-interface Status {
-  title: string;
-  image: string;
-  buttonTitle?: string;
-}
 @Component({
   selector: 'app-seguimiento',
   templateUrl: './seguimiento.page.html',
@@ -21,21 +16,6 @@ export class SeguimientoPage implements OnInit {
     this.navCtrl.back();
   }
 
-  eventosinicio: { title: string, image: string ,buttonTitle?: string}[] = [
-    { title: 'Iniciado', image:'assets/images/inicio.jpg',buttonTitle: 'Actual' },
-  ];
-  eventosprogreso: { title: string, image: string ,buttonTitle?: string}[] = [
-    { title: 'En progreso', image:'assets/images/inicio.jpg',buttonTitle: 'Próximo...' },
-  ];
-  eventosListo: { title: string, image: string ,buttonTitle?: string}[] = [
-    { title: 'Listo para entrega', image:'assets/images/inicio.jpg',buttonTitle: 'Próximo...' },
-  ];
-
-
-  onButtonClick(evento: Status) {
-    console.log( evento.title);
-
-  }
   presentAlert(){
     console.log('Acción Finalizar');
   }
