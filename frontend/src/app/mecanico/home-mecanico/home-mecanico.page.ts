a7453b98839229f3e654fb37837387f3c20f8f7c
Isaac Bravo <is.bravo@duocuc.cl>
2024-11-08 16:23:39 -0300

    home mecanico terminado

--- frontend/src/app/mecanico/home-mecanico/home-mecanico.page.ts
+++ frontend/src/app/mecanico/home-mecanico/home-mecanico.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { ManteciService } from 'src/app/services/manteci.service';
+import { Storage } from '@ionic/storage-angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-mecanico',
@@ -12,10 +14,15 @@ export class HomeMecanicoPage implements OnInit {
   token: string | null = null;
   user: any = {};
   calendar: any[] = [];
-  currentUser: any = {};  // User object to store the current client
-  currentList: any[] = [];  // To store the current maintenance records list
+  currentUser: any = {};
+  currentList: any[] = [];
 
-  constructor(private userService: UserService, private manteciService: ManteciService) {}
+  constructor(
+    private userService: UserService,
+    private manteciService: ManteciService,
+    private storage: Storage,
+    private router: Router
+  ) {}
 
   async ngOnInit() {
     const sessionData = await this.userService.getUserSession();
@@ -27,11 +34,15 @@ export class HomeMecanicoPage implements OnInit {
     const data = await this.manteciService.getMaintenanceCalendar();
     if (data) {
       this.calendar = data.calendar;
-      this.currentList = data.current; // Store the current maintenance records
-      this.currentUser = this.currentList[0] ? this.currentList[0].client : null; // Example for the first client
-      console.log(data)
+      this.currentList = data.current;
+      this.currentUser = this.currentList[0] ? this.currentList[0].client : null;
     } else {
       console.error('No se pudo obtener el calendario de mantenimiento.');
     }
+  }
+
+  async saveMaintenanceIdAndNavigate(maintenanceId: number) {
+    await this.storage.set('idmantesion', maintenanceId);
+    this.router.navigate(['/mecanico/info-ser-cli']); // Change to your target route
   }
 }
