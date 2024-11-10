9b204d074e99f14f0b5b58d0ea6b402f64c89d53
Ignacio Toledo <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-11-10 20:45:20 -0300

    fixes endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,8 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 require __DIR__ . '/api/UserRoutes.php';
-require __DIR__ . '/api/CarRoutes.php';
 require __DIR__ . '/api/CarBrandsRoutes.php';
+require __DIR__ . '/api/CarRoutes.php';
 require __DIR__ . '/api/ServiceRoutes.php';
 require __DIR__ . '/api/MaintenanceRoutes.php';
 require __DIR__ . '/api/CarModelsRoutes.php';
