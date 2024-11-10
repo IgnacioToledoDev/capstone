021bcd1fcb1de32ff795c0c8f0378aee41d6c71d
Ignacio Toledo <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-11-10 17:55:24 -0300

    feat - add button in spanish

--- backend/app/Filament/Resources/CarBrandResource/Pages/ListCarBrands.php
+++ backend/app/Filament/Resources/CarBrandResource/Pages/ListCarBrands.php
@@ -15,7 +15,7 @@ class ListCarBrands extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()->label('Agregar marca'),
         ];
     }
 }
