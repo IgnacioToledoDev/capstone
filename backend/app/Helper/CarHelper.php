651816f539fa0368aeadeaab53daed672e74fd46
Ignacio Toledo <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-11-18 21:41:28 -0300

    fix helper

--- backend/app/Helper/CarHelper.php
+++ backend/app/Helper/CarHelper.php
@@ -9,16 +9,26 @@ use App\Models\CarModel;
 class CarHelper
 {
 
-    public function getCarBrandName($carBrandId): string
+    public function getCarBrandName($carId): string
     {
-        $car = Car::whereId($carBrandId)->first();
+        $car = Car::whereId($carId)->first();
+        if (!$car && !isset($car->brand_id)) {
+            $brand = CarBrand::whereId($carId)->first();
+            return $brand->name;
+        }
+
         $brandName = CarBrand::whereId($car->brand_id)->first();
         return $brandName->name ?? 'not found name';
     }
 
-    public function getCarModelName($carModelId): string
+    public function getCarModelName($carId): string
     {
-        $car = Car::whereId($carModelId)->first();
+        $car = Car::whereId($carId)->first();
+        if (!$car && !isset($car->model_id)) {
+            $brand = CarModel::whereId($car->model_id)->first();
+            return $brand->name;
+        }
+
         $modelName = CarModel::where(['id'=> $car->model_id])->first()->name;
         return $modelName ?? 'not found name';
     }
