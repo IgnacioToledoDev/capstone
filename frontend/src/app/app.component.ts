eff96edcb30d6fa0a6201737c6b83d585f52357d
Isaac Bravo <is.bravo@duocuc.cl>
2024-11-27 18:59:16 -0300

    v1 app blanco

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  
   constructor() {}
+
+  toggleDarkMode(isDarkMode: boolean) {
+    document.body.classList.toggle('dark-theme', isDarkMode);
+}
+  
 }
